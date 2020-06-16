13f66dd9ad5a45afb43382c3d67c36591d6b3b3d
Noteworthy A Cappella LHS <38771589+NoteworthyAcappella@users.noreply.github.com>
2020-06-16 19:55:17 -0400

    same message as last time - comma should be semicolon

--- trialLoader.js
+++ trialLoader.js
@@ -6,10 +6,10 @@ function trialLoadImages(){
       for(var i=1; i<obj.ninteentwenty.length; i++){
         document.getElementById("19-20").innerHTML += "<img src=\"2019-20/" + obj.ninteentwenty[i] + ".jpg\" style=\"margin:30px;\" />"
       }
-      for(var i=1; i<obj.eighteennineteen.length, i++){
+      for(var i=1; i<obj.eighteennineteen.length; i++){
         document.getElementById("18-19").innerHTML += "<img src=\"2018-19/" + obj.ninteentwenty[i] + ".jpg\" style=\"margin:30px;\" />"
       }
-      for(var i=1; i<obj.seventeeneighteen.length, i++){
+      for(var i=1; i<obj.seventeeneighteen.length; i++){
         document.getElementById("17-18").innerHTML += "<img src=\"20c17-18/" + obj.ninteentwenty[i] + ".jpg\" style=\"margin:30px;\" />"
       }
     }
