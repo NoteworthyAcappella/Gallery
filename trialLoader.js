a17b8e62e22970d0596a0c1b906cdff9183a6a89
Noteworthy A Cappella LHS <38771589+NoteworthyAcappella@users.noreply.github.com>
2020-06-25 09:10:42 -0400

    Update trialLoader.js

--- trialLoader.js
+++ trialLoader.js
@@ -1,23 +1,20 @@
 function trialLoadImages(){
   var imageList = new XMLHttpRequest();
-  imageList.onReadyStateChange = function(){
-    if(this.readyState == 4 && this.status == 200){
+  imageList.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200) {\
       var obj = JSON.parse(this.responseText);
-      for(var i=0; i<obj.nineteentwenty.length; i++){
-        document.getElementById("19-20").innerHTML += '<img src="./2019-20/' + obj.nineteentwenty[i] + '.jpg" style="margin:30px;" />';
+      for(var i=0; i < obj.nineteentwenty.length; i++) {
+        document.getElementById("19-20").innerHTML += "<img src=\"2019-20/" + obj.nineteentwenty[i] + ".jpg\" style=\"margin:30px;\" />";
       }
-      for(var i=0; i<obj.eighteennineteen.length; i++){
-        document.getElementById("18-19").innerHTML += '<img src="./2018-19/' + obj.eighteennineteen[i] + '.jpg" style="margin:30px;" />';
+      for(var i=0; i < obj.eighteennineteen.length; i++) {
+        document.getElementById("18-19").innerHTML += "<img src=\"2018-19/" + obj.eighteennineteen[i] + ".jpg\" style=\"margin:30px;\" />";
       }
-      for(var i=0; i<obj.seventeeneighteen.length; i++){
-        document.getElementById("17-18").innerHTML += '<img src="./20c17-18/' + obj.seventeeneighteen[i] + '.jpg" style="margin:30px;" />';
+      for(var i=0; i < obj.seventeeneighteen.length; i++) {
+        document.getElementById("17-18").innerHTML += "<img src=\"2017-18/" + obj.seventeeneighteen[i] + ".jpg\" style=\"margin:30px;\" />";
       }
     }
-    else{
-      document.getElementById("all").innerHTML += "<h1>Failed to load images. Please try again later.</h1>";
-    }
-  }
-  imageList.open("GET","images.json",true);
+  };
+  imageList.open("GET", "https://www.noteworthylhs.ml/Gallery/images.json", true);
   imageList.send();
 }
 
